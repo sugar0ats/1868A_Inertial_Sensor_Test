b5835e22e1f3a9948fffa308545786ca794056e9
emma <emmacao1234@gmail.com>
2023-12-27 11:55:06 -0800

    Initial commit

---
+++ include/robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+extern controller Controller1;
+extern motor_group LeftMotor;
+extern motor_group RightMotor;
+extern motor rightmotorA;
+extern motor rightmotorM;
+extern motor rightmotorB;
+extern motor leftmotorA;
+extern motor leftmotorM;
+extern motor leftmotorB;
+extern motor Catapult;
+extern motor Intake;
+extern digital_out wings;
+extern inertial inertialSensor;
+extern rotation rotationSensor;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
---
+++ src/robot-config.cpp
@@ -0,0 +1,31 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+// A global instance of brain used for printing to the V5 brain screen
+brain Brain;
+controller Controller1;
+motor rightmotorA = motor(PORT16, ratio6_1, false);
+motor rightmotorB = motor(PORT17, ratio6_1, false);
+motor rightmotorM = motor(PORT13, ratio6_1, true);
+motor_group RightMotor = motor_group(rightmotorA, rightmotorM, rightmotorB);
+motor leftmotorA = motor(PORT19, ratio6_1, true);
+motor leftmotorM = motor(PORT10,ratio6_1, false);
+motor leftmotorB = motor(PORT20, ratio6_1, true);
+motor_group LeftMotor = motor_group(leftmotorA, leftmotorM, leftmotorB);
+motor Intake = motor(PORT11, ratio6_1, true);
+motor Catapult = motor(PORT18, ratio36_1, false);
+digital_out wings = digital_out(Brain.ThreeWirePort.A);
+inertial inertialSensor = inertial(PORT15);
+rotation rotationSensor = rotation(PORT12);
+
+bool RemoteControlCodeEnabled = true;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void) {
+  // Nothing to initialize
+}
